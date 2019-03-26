54dfcf81ee0bb25a6b310b4baacc9e976dbf5a5e
unknown <EX_FUGX@cn.midea.com>
2019-03-26 10:47:08 +0800

    cherry-pick dev one

--- src/pages/MyReception/main.vue
+++ src/pages/MyReception/main.vue
@@ -1,7 +1,8 @@
 <template>
     <div>
        <input type="file" name="file" id="file" @change="changeFile">
-
+        <div>我是dev分支增加的内容，改一下啊看看</div>
+        <div>cherry pick one</div>
     </div>
 </template>
 <script>
